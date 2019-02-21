d25a1573916722ea982f6f2604596cd27d5d6943
LinZhuming <9208608@qq.com>
2019-02-21 19:44:29 +0800

    修改菜品路由结果

--- routes/admin/category.js
+++ routes/admin/category.js
@@ -51,7 +51,11 @@ router.post('/', (req, res) => {
   var data = req.body;
   pool.query('INSERT INTO cat_category SET ?', data, (err, result) => {
     if (err) throw err;
-    res.send({ code: 200, msg: '1 category added' });
+    res.send({ 
+			code: 200,
+			msg: '1 category added',
+			cid: result.insertId		
+		});
   })
 })
 
